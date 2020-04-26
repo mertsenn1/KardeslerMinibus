881f80ad6745e5ef69f2a180b65ffe080a6ab932
edemirkirkan <63519745+edemirkirkan@users.noreply.github.com>
2020-04-27 01:07:18 +0300

    Add Settings Layout

---
+++ app/src/main/java/com/example/passengersettings/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.passengersettings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigation;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+    }
+}
