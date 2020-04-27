35ab21439c75f1044a4208ca28ba595019194c0f
SebnemTurkoglu <sebnem.turkoglu@ug.bilkent.edu.tr>
2020-04-27 23:56:51 +0300

    27.04.2020 v0.2 sign up merge

--- app/src/main/java/com/example/comingsoon/SignUpActivity.java
+++ app/src/main/java/com/example/comingsoon/SignUpActivity.java
@@ -2,7 +2,10 @@ package com.example.comingsoon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Button signinButton = findViewById(R.id.signinButton2);
+
+        signinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( SignUpActivity.this, SigninActivity.class);
+                startActivity( intent);
+            }
+        });
     }
 }
