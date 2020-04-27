ef9f3d962cfac4dfe8e15e6b8753d43615b79cf7
SebnemTurkoglu <sebnem.turkoglu@ug.bilkent.edu.tr>
2020-04-27 17:59:34 +0300

    27.04.2020 update

---
+++ app/src/main/java/com/example/comingsoon/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.comingsoon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        configureNextButton();
+    }
+
+    private void configureNextButton() {
+        Button buttonNext = (Button) findViewById( R.id.buttonNext);
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity( new Intent( MainActivity.this, SearchTab.class) );
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/comingsoon/SearchTab.java
@@ -0,0 +1,14 @@
+package com.example.comingsoon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchTab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_tab);
+    }
+}
