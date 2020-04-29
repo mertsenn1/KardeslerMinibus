6852eddab4b5acdd3dd62a7169c85ed1917aaf8d
SebnemTurkoglu <sebnem.turkoglu@ug.bilkent.edu.tr>
2020-04-29 16:21:07 +0300

    27.04.2020 v0.2 sign up merge

--- app/src/main/java/com/example/comingsoon/SettingsActivity.java
+++ app/src/main/java/com/example/comingsoon/SettingsActivity.java
@@ -9,31 +9,31 @@ import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class SearchTab extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
-        BottomNavigationView bottomNav;
+    BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_tab);
+        setContentView(R.layout.activity_settings);
 
         bottomNav = findViewById( R.id.bottom_navigation);
-        bottomNav.setSelectedItemId( R.id.search);
+        bottomNav.setSelectedItemId( R.id.settings);
 
         bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch ( item.getItemId()) {
                     case R.id.search:
+                        startActivity( new Intent( getApplicationContext(), SearchTab.class));
+                        overridePendingTransition( 0, 0);
                         return true;
                     case R.id.map:
                         startActivity( new Intent( getApplicationContext(), MapActivity.class));
                         overridePendingTransition( 0, 0);
                         return true;
                     case R.id.settings:
-                        startActivity( new Intent( getApplicationContext(), SettingsActivity.class));
-                        overridePendingTransition( 0, 0);
                         return true;
                 }
                 return false;
